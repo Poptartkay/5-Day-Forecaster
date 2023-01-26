140197a47a37a815167de09aee128c51da5ee669
Poptartkay <fordjulietkay@gmail.com>
2023-01-26 13:46:41 -0800

    added more variables as well as if/else/else if statements

--- Assets/script.js
+++ Assets/script.js
@@ -12,4 +12,128 @@ $('.search').on("click", function(event){
     getForecastToday();
     getSearchHistory();
 });
-var weatherForecastEl = $('.weatheForecast');
+var weatherForecastEl = $('.weatherForecast');
+
+
+function getweatherForecast() {
+	var getweatherForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIkey}`;
+
+    $.ajax({
+        url: getweatherForecastURL,
+        method: 'GET',
+    }).then(function (response){
+        var weekArray = response.list;
+        var myWeather = [];
+        $.each(weekArray, function (index, value){
+            testObj = {
+                currentDate: value.dt_txt.split(' ')[0],
+                time: value.dt_txt.split(' ')[1],
+                temp: value.main.temp,
+                // feels_like: value.main.feels_like,
+                icon: value.weather[0].icon,
+                humidity: value.main.humidity
+            }
+
+            if (value.dt_txt.split(' ')[1] === "15:00:00"){
+
+            }
+        
+        })
+        for (let i = 0; i < myWeather.length; i++){
+
+            var divElCard = $('<div>');
+            divElCard.attr('class', 'card text-white mb-3 cardOne');
+            divElCard.attr('style', 'max-width: 200px');
+            weatherForecastEl.append(divElCard);
+
+            vardivElBody = $('<div>');
+            divElHeader.attr('class', 'cardHeader')
+            var m = moment(`${myWeather[i].currentDate}`).format('MM/DD/YYYY');
+            divElHeader.text(m);
+            divElCard.append(divElHeader)
+
+            var divElBody = $('<div>');
+            divElBody.attr('class', 'cardBody');
+            divElCard.append(divElBody);
+
+            var divElIcon = $('<img>');
+            divElIcon.attr('class', 'weatherIcons');
+            divElIcon.attr('src', `https://openweathermap.org/img/wn/${myWeather[i].icon}@2x.png`);
+            divElBody.append(divElIcon);
+
+            var pElTemperature = $('<p>').text(`Temperature ${myWeather[i].temp}°F`);
+            divElBody.append(pElTemperature);
+
+            // var pElFeelsLike = $('<p>').text(`Feels Like: ${myWeather[i].feels_like} °F`);
+			// divElBody.append(pElFeelsLike);
+
+            var pElHumidity = $('<p>').text(`Humidity: ${myWeather[i].humidity} %`);
+			divElBody.append(pElHumidity);
+
+        }
+    })
+}
+var weatherDetails = $('.weatherDetails')
+
+
+
+function getForecastToday() {
+	var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIkey}`;
+
+    $(weatherDetails).empty();
+
+
+    $ajax({
+        url: getUrlCurrent,
+        method: 'GET',
+    }).then(function (response){ 
+        $('.weatherCity').text(response.name);
+        $('.weatherDate').text(currentDate);
+
+
+
+        var cityLon = response.coord.lon;
+        var cityLat = response.coord.lat;
+        $('.weatherIcons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
+
+
+        var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
+        weatherDetails.append(pEl);
+
+
+        var pElFeelsLike = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
+        weatherDetails.append(pElFeelsLike);
+
+
+        var pElHumidity = $('<p>').text(`Humidity: ${ response.main.humidity} %`);
+        weatherDetails.append(pElHumidity);
+
+        var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
+        weatherDetails.append(pElWind);
+
+        var getUrlSunny = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,daily,minutely&appid=${APIkey}`;
+
+
+
+
+        $.ajax ({
+            url:getUrlSunny,
+            method: 'GET',
+        }).then(function (response) {
+            var pElUvi = $('<p>').text(`UV Index: `);
+            var uviSpan = $( '<span>').text(response.current.uvi);
+            var uvi = response.current.uvi;
+            pElUvi.append(pELUvi);
+            if (uvi >= 0 && uvi <= 2) {
+                uviSpan.attr('class', 'favorable');
+            } else if (uvi > 2 && uvi <= 5) {
+                uviSpan.attr("class", "moderate")
+            } else if (uvi > 5 && uvi <= 7) {
+                uviSpan.attr("class", "severe")
+            } else {
+                uviSpan.attr("class", "undefinedUVI")
+            }
+        });
+    });
+    getweatherForecast();
+}
--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # 5-Day-Forecaster
+
+## Table of contents
+​
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+​
+​
+​
+### The challenge
+​
+User Story -
+ AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+
+Acceptance Criteria -
+
+​GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+​
+### Links
+​
+
+- Live Site URL: 
+- GitHub Repo : 
+
+​
+### Built with
+​
+- HTML
+- CSS
+- JavaScript
+- JQuery
+
+​
+### What I learned
+
+I learned how to properly utilize and link API's to show correct weather predictions. As well as
+how to create a proper weather dashboard. I will admit there were some mild challenges here, but 
+overall I feel pretty good about this challenge.
+
+​
+
+​
+### Continued development
+If there was anything I could differently is I would style the app differently.
+(This would just be a personal preference of mine and I might come back to the challenge to do so)
+
+​
+## Author
+Kay Ford
+
+
